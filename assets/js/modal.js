7ec11420a20990decc40178d53cdb7f28cc4b88b
SotaDesktop <kansota1125@gmail.com>
2025-05-10 21:25:11 +0900

    update

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -4,6 +4,26 @@ let modal = document.getElementById("modal");
 let overlay = modal.querySelector(".overlay");
 let modalContents = modal.querySelectorAll(".modal-content");
 
+// 開く
+let openBtns = document.querySelectorAll(".btn-open-modal[data-modal]");
+
+openBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let target = btn.getAttribute("data-modal");
+    let targetContent = modal.querySelector(
+      `.modal-content[data-modal="${target}"]`
+    );
+
+    if (targetContent) {
+      modal.classList.add("is-show");
+      targetContent.classList.add("is-active");
+    }
+  });
+});
+
+// 閉じる
 modalContents.forEach((content) => {
   let btnClose = content.querySelectorAll(".btn-close-modal");
 
@@ -20,7 +40,7 @@ closeBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
     let length = modal.querySelectorAll(".modal-content.is-active").length;
 
-    console.log(length)
+    console.log(length);
 
     if (length === 0) {
       modal.classList.remove("is-show");
